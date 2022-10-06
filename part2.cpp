7d8f445b30620c63ff1417b27a4a0461e5d1e163
Arya Singh <92276734+AryaDSingh@users.noreply.github.com>
2022-10-06 14:52:51 -0700

    Add files via upload

---
+++ part2.cpp
@@ -0,0 +1,74 @@
+
+#include <iostream>
+#include "List.h"
+#include "Vector2.h"
+/*Arya Singh*/
+
+double elapsed_time( clock_t start, clock_t finish){
+   // returns elapsed time in milliseconds
+   return (finish - start)/(double)(CLOCKS_PER_SEC/1000);
+}
+
+
+int main() {
+
+  //number of elements to be tested
+  int amount_ele = 1000000;
+ //data types comment out ones not being used for compiler errors
+ //int int_type = 1;
+ //float float_type = 1.111;
+  char char_type = 'a';
+  
+
+  //Vector time variables
+  clock_t startVec_insert, finishVec_insert, startVec_visit, finishVec_visit ;// used for getting the time.
+  
+  //start time for insertion
+  startVec_insert = clock();
+  //for VECTOR insertion
+  Vector<int> vect;
+  for (int i = 0; i < amount_ele; i ++) {
+    vect.push_front(char_type);
+  }
+  //stop time
+  finishVec_insert = clock();
+  
+  //for VECTOR visiting
+  startVec_visit = clock();
+  vect.visitAll();
+  
+  //stop time
+  finishVec_visit = clock();
+  double time_taken_Vec_insert = elapsed_time(startVec_insert,finishVec_insert);
+  double time_taken_Vec = elapsed_time(startVec_visit,finishVec_visit);
+  
+  std::cout<<"time taken for vector insertion "<<time_taken_Vec_insert<<"\n";
+  std::cout<<"time taken for vector visiting "<<time_taken_Vec<<"\n";
+  
+  //list time variables
+  clock_t startList_insert, finishList_insert, startList_visit, finishList_visit ;// used for getting the time.
+  
+  //start time for insertion
+  startList_insert = clock();
+
+  //for LIST insertion
+  List<int> list;
+  for (int y = 0; y < amount_ele; y ++) {
+    list.push_front(char_type);
+  }
+  //stop time
+  finishList_insert =clock();
+  
+  //for LIST visiting
+  startList_visit = clock();
+  list.visitAll();
+  //stop time
+  finishList_visit = clock();
+  double time_taken_List_insert = elapsed_time(startList_insert, finishList_insert);
+  double time_taken_List = elapsed_time(startList_visit, finishList_visit);
+  
+  std::cout<<"time taken for list insertion "<<time_taken_List_insert<<"\n";
+  std::cout<<"time taken for list visiting "<<time_taken_List<<"\n";
+}
+
+
